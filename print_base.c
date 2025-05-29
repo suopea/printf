8fe5974a851cb30b23373083809a6f37a974a14b
suopea <suopea@pm.me>
2025-05-29 17:13:03 +0300

    print base redone

--- print_base.c
+++ print_base.c
@@ -6,96 +6,57 @@
 /*   By: ssuopea <ssuopea@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 14:54:46 by ssuopea           #+#    #+#             */
-/*   Updated: 2025/05/29 13:59:51 by ssuopea          ###   ########.fr       */
+/*   Updated: 2025/05/29 17:11:46 by ssuopea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "ft_printf.h"
 
-static int	invalid_base(char *base);
-static void	store_radix(char *base, long *radix);
-static void	handle_negatives(long *temp);
-static long	expo(long base, long exponent);
-
-void	print_base(int nbr, char *base)
+int	ft_strlen(char *str)
 {
-	long	radix;
-	long	pos;
-	long	multiple;
+	int i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	write_reverse(char *buffer, int len)
+{
+	char	out[24];
+	int		index_out;
+
+	index_out = 0;
+	while (index_out <= len)
+	{
+		out[index_out] = buffer[len - index_out - 1];
+		index_out++;
+	}
+	return (write(1, out, len));
+}
+
+int	print_base(int nbr, char *base)
+{
+	char	buffer[24];
+	int		i;
+	int		positive;
 	long	temp;
 
-	if (invalid_base(base))
-		return ;
+	if (nbr == 0)
+		return (write(1, "0", 1));
+	i = 0;
+	positive = 1;
 	temp = nbr;
-	pos = 1;
-	store_radix(base, &radix);
-	handle_negatives(&temp);
-	while (temp >= expo(radix, pos))
-		pos++;
-	while (pos > 1)
+	if (temp < 0)
 	{
-		multiple = radix - 1;
-		while (temp < expo(radix, pos - 1) * multiple && multiple != 0)
-			multiple--;
-		write(1, &base[multiple], 1);
-		temp -= expo(radix, pos - 1) * multiple;
-		pos--;
+		positive = 0;
+		temp = -temp;
 	}
-	write(1, &base[temp], 1);
-}
-
-static int	invalid_base(char *base)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (base[i])
+	while (temp > 0)
 	{
-		j = 1;
-		if (base[i] == '+' || base[i] == '-')
-			return (1);
-		while (base[i + j])
-		{
-			if (base[i] == base [i + j])
-				return (1);
-			j++;
-		}
-		i++;
+		buffer[i++] = base[temp % ft_strlen(base)];
+		temp /= ft_strlen(base);
 	}
-	if (i < 2)
-		return (1);
-	return (0);
-}
-
-static void	store_radix(char *base, long *radix)
-{
-	long	i;
-
-	i = 0;
-	while (base[i])
-		i++;
-	*radix = i;
-}
-
-static long	expo(long base, long exponent)
-{
-	long	out;
-
-	out = base;
-	while (exponent - 1)
-	{
-		out *= base;
-		exponent--;
-	}
-	return (out);
-}
-
-static void	handle_negatives(long *temp)
-{
-	if (*temp < 0)
-	{
-		write(1, "-", 1);
-		*temp = -*temp;
-	}
+	if (!positive)
+		buffer[i++] = '-';
+	return (write_reverse(buffer, i--));
 }
